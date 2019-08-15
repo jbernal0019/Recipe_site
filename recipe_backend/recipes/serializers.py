f98821aef93b4980b070dd245cbbdd6695a36f56
Jorge <jbernal0019@yahoo.es>
2019-08-15 07:41:23 -0400

    Override RecipeSerializer.create method to raise a validation error if a user attempts to create more than

--- recipe_backend/recipes/serializers.py
+++ recipe_backend/recipes/serializers.py
@@ -16,6 +16,17 @@ class RecipeSerializer(serializers.HyperlinkedModelSerializer):
         model = Recipe
         fields = ('url', 'id', 'name', 'owner_username', 'ingredients', 'steps', 'owner')
 
+    def create(self, validated_data):
+        """
+        Overriden to raise a validation error if a user attempts to create more than
+        one recipe.
+        """
+        owner = validated_data.get('owner')
+        if hasattr(owner, 'recipe'):
+            raise serializers.ValidationError(
+                {'non_field_errors': ["User can only create a single recipe."]})
+        return super(RecipeSerializer, self).create(validated_data)
+
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
     recipe = serializers.HyperlinkedRelatedField(view_name='recipe-detail',
